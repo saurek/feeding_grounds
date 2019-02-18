2df96abdcce6d49cf4aed5643d8be33be170af69
saurek <s.reklaitis@gmail.com>
2019-02-18 23:48:10 +0200

    8 - Context in Django Templates

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,10 +1,32 @@
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
-import random
 
 
-# Create your views here.
-# function based view
 def home(request):
-    html_var = str(random.randint(0, 1000)) + ' tanks '
-    return render(request, "base.html", {"html_var": html_var})  # response
+    num = None
+    some_list = [
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000)
+    ]
+    condition_bool_item = False
+    if condition_bool_item:
+        num = random.randint(0, 100000000)
+    context = {
+        "num": num,
+        "some_list": some_list
+    }
+    return render(request, "base.html", context)
