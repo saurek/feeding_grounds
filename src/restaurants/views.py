842c6bbef33a2f4bf3f085239f5e990a29c4bd09
saurek <s.reklaitis@gmail.com>
2019-02-19 19:04:18 +0200

    9 - Template Inheritance

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -9,17 +9,6 @@ def home(request):
         random.randint(0, 100000000),
         random.randint(0, 100000000),
         random.randint(0, 100000000),
-        random.randint(0, 100000000),
-        random.randint(0, 100000000),
-        random.randint(0, 100000000),
-        random.randint(0, 100000000),
-        random.randint(0, 100000000),
-        random.randint(0, 100000000),
-        random.randint(0, 100000000),
-        random.randint(0, 100000000),
-        random.randint(0, 100000000),
-        random.randint(0, 100000000),
-        random.randint(0, 100000000),
         random.randint(0, 100000000)
     ]
     condition_bool_item = False
@@ -29,4 +18,16 @@ def home(request):
         "num": num,
         "some_list": some_list
     }
-    return render(request, "base.html", context)
+    return render(request, "home.html", context)
+
+
+def about(request):
+    context = {
+    }
+    return render(request, "about.html", context)
+
+
+def contact(request):
+    context = {
+    }
+    return render(request, "contact.html", context)
